682e98217cd68ca92f9387a69058d33611fcefd8
Ola <lepkowska.ola@gmail.com>
2021-11-30 20:53:07 +0100

    added "mark_as_completed" function

--- project1.py
+++ project1.py
@@ -9,18 +9,19 @@ mydb = mysql.connector.connect(
     password="",
     database="mydatabase"
 )
+
 mycursor = mydb.cursor()
 
 
 def get_tasks_from_database():
-    sql = "SELECT id, title, taskDate FROM tasks"
+    sql = "SELECT id, title, taskDate, task_status FROM tasks"
     mycursor.execute(sql)
     my_tasks = mycursor.fetchall()
 
     tasks = []
 
-    for task_id, task_title, task_date in my_tasks:
-        task = {"id": task_id, "title": task_title, "date": task_date}
+    for task_id, task_title, task_date, task_status in my_tasks:
+        task = {"id": task_id, "title": task_title, "date": task_date, "status": task_status}
         tasks.append(task)
 
     return tasks
@@ -34,9 +35,17 @@ def display_tasks(tasks):
     for key, value in groupby(tasks, key=operator.itemgetter("date")):
         # print the date the day of the week
         print(str(key) + " " + calendar.day_name[key.weekday()])
+
         # print title of the task
+        completed_title = ""
         for i in value:
-            print("\t" + i["title"])
+            # if status is 'Completed' print the crossed out title, else print normally
+            if i['status'] == "Completed":
+                for x in i['title']:
+                    completed_title = completed_title + x + '\u0336'
+                print(completed_title)
+            else:
+                print("\t" + i["title"])
         print(30 * "-")
 
 
@@ -50,7 +59,7 @@ def display_menu():
     print("4) Mark as completed")
 
     choice = " "
-    acceptable_range = range(0, 4)
+    acceptable_range = range(0, 5)
     within_range = False
 
     # check if input is not empty, is numeric and is in range
@@ -79,13 +88,12 @@ def display_menu():
     elif choice == 3:
         edit_task()
     elif choice == 4:
-        mark_as_completed()
+        mark_as_completed(current_tasks)
 
 
 def add_new_task():
     new_task_title = ""
     new_task_date = None
-    new_task_status = ""
 
     # check if input is not empty
     while not (new_task_title and new_task_title.strip()):
@@ -143,8 +151,35 @@ def edit_task():
     pass
 
 
-def mark_as_completed():
-    pass
+def mark_as_completed(tasks):
+    task_id = " "
+    acceptable_ids = []
+    within_range = False
+
+    # get IDs of tasks
+    for obj in tasks:
+        acceptable_ids.append(obj['id'])
+
+    # check if input is not empty, is numeric and task with the ID exist
+    while not task_id.isnumeric() or len(task_id) == 0 or not within_range:
+
+        task_id = input("Enter ID of the task you want to mark as completed:")
+
+        # numeric check
+        if not task_id.isnumeric():
+            print("Input is not a number or is empty")
+
+        # range check
+        if task_id.isnumeric():
+            if int(task_id) in acceptable_ids:
+                within_range = True
+            else:
+                print("There is no task with this ID")
+                within_range = False
+
+    mycursor.execute(f"UPDATE tasks SET task_status = 'Completed' WHERE id = {task_id}")
+    mydb.commit()
+    print(f'record with ID {task_id} marked as completed')
 
 
 # display warning if it's 7 or less days to task date
